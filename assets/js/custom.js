0eed8291cc81d4aa773c9d7947af8c190ce463bb
Musharrof Shishir <musharrofshishir@users.noreply.github.com>
2020-12-23 13:26:59 +0600

    testing instagram api

---
+++ assets/js/custom.js
@@ -0,0 +1,14 @@
+
+
+//! Instagram API Testing
+
+// $(window).on('load', function(){
+
+//     $.instagramFeed({
+//         'username':'ui.colour',
+//         'container': '#instaPhotos',
+//         'items_per_row': 3,        
+//         'display_gallery':true
+//         // 'display_profile':false
+//     });
+// });
